d621bd614debe41627925706a0f522e5742811a7
Jay <45482664+BailyForTest@users.noreply.github.com>
2024-08-05 15:03:57 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------------
+# @Project : cms_feed
+# @Time    : 2024/8/5 14:50
+# @Author  : bj
+# @Email   : 475829130@qq.com
+# @File    : test.py
+# @Software: PyCharm
+print("test")
